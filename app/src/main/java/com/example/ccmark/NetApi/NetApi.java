c53602c66b6034c66d9c10db282524c1bad1bacc
markcc <970518209@qq.com>
2017-03-12 00:27:38 +0800

    重构代码 添加判断条件 appcode处理

---
+++ app/src/main/java/com/example/ccmark/NetApi/NetApi.java
@@ -0,0 +1,11 @@
+package com.example.ccmark.NetApi;
+
+/**
+ * Created by ccmark on 2017/3/11.
+ */
+
+public class NetApi {
+    //空气质量API appcode
+    //注意：每次Pull后需手动修改正确的appcode值
+    public static String APPCODE = "APPCODE 7e015486fcda437389317dca15f5xxxx";
+}
